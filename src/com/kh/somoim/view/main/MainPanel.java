1f8970afc489ad57e0c5cae5866373571d90a4cc
ko <ko@DESKTOP-7MDR7BJ>
2019-04-24 15:52:22 +0900

    test2

--- src/com/kh/somoim/view/main/MainPanel.java
+++ src/com/kh/somoim/view/main/MainPanel.java
@@ -20,6 +20,7 @@ public class MainPanel extends JPanel{
 		System.out.println("mainPanel in");
 		
 		// 아이콘 넣기!
+		System.out.println("19038120938123091");
 		
 		// 상단/중단/하단 패널 생성
 		topPanel = new TopPanel();
