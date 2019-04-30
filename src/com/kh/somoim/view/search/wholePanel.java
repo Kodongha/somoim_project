431bfdfbc207965929cc17edc8a38071637c2bc4
user <user@DESKTOP-DIHEKLC>
2019-05-01 08:50:19 +0900

    수정

--- src/com/kh/somoim/view/search/wholePanel.java
+++ src/com/kh/somoim/view/search/wholePanel.java
@@ -4,11 +4,23 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 
+import com.kh.somoim.login.model.vo.MemberVO;
+import com.kh.somoim.view.main.CenterPanel;
+import com.kh.somoim.view.main.MainPanel;
+import com.kh.somoim.view.mainFrame.MainFrame;
+
 public class wholePanel extends JPanel {
 	SearchFavorite searchFavorite;
 	 SearchAllFavorite searchAllFavorite;
+	 
+	 MainFrame mainFrame;
+	 CenterPanel centerPanel;
+	 MainPanel mainpanel;
+	 
+				SearchPanel searchPanel;
+				MemberVO memberVO;
 	
-	public wholePanel() {
+	public wholePanel( MainFrame mainFrame,CenterPanel centerPanel, MainPanel mainpanel,SearchPanel searchPanel,MemberVO memberVO) {
 	this.setBackground(Color.white);
 	this.setLayout(null);
 	
@@ -20,7 +32,7 @@ public class wholePanel extends JPanel {
 	
 	
 	
-	 searchAllFavorite= new SearchAllFavorite();
+	 searchAllFavorite= new SearchAllFavorite(mainFrame, centerPanel, mainpanel,searchPanel, this,  memberVO);
 	 searchAllFavorite.setLocation(10, 160);
 	 searchAllFavorite.setSize(450, 500);
 	 
